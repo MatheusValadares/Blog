ceb9216126c4dab49690d21beb9fb2bb15427829
matheus <matheusvaladares1909@gmail.com>
2024-03-07 10:00:08 -0300

    instalação do mongoose

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require('path');
 const router = require('./routes/router');
 const PORT = 5000;
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/blog');
+let db = mongoose.connection;
+db.on('error', () => { console.log(`Houve um erro no db`) });
+db.once('open', () => { console.log('DB carregado') })
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use('/assets', express.static(path.join(__dirname, 'views/assets')));
@@ -12,4 +18,5 @@ app.set('view engine', 'ejs');
 
 app.use('/', router);
 
-app.listen(PORT, ()=>{console.log(`Server running in port ${PORT}`)})  
+
+app.listen(PORT, () => { console.log(`Server running in port ${PORT}`) })  
